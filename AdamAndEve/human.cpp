3c2daaa15caa5ad7c8172c856a8b31357494fde7
Mohamed Amine Bergach <Amine@Rasmusen.local>
2015-02-05 19:29:52 +0100

    adding human files just to bootstrap

---
+++ AdamAndEve/human.cpp
@@ -0,0 +1,9 @@
+//
+//  human.cpp
+//  AdamAndEve
+//
+//  Created by Mohamed Amine Bergach on 05/02/2015.
+//  Copyright (c) 2015 Mohamed Amine Bergach. All rights reserved.
+//
+
+#include "human.h"
---
+++ AdamAndEve/human.h
@@ -0,0 +1,22 @@
+//
+//  human.h
+//  AdamAndEve
+//
+//  Created by Mohamed Amine Bergach on 05/02/2015.
+//  Copyright (c) 2015 Mohamed Amine Bergach. All rights reserved.
+//
+
+#ifndef __AdamAndEve__human__
+#define __AdamAndEve__human__
+
+#include <stdio.h>
+
+class human{
+public:
+    
+private:
+    int hid;
+    int sex;
+};
+
+#endif /* defined(__AdamAndEve__human__) */
